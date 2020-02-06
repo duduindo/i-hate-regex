02720f0745e2928dda5415b5a630963e6d9ce797
Eduardo Paixão <duduindo@gmail.com>
2020-02-05 22:39:57 -0300

    Files restore to version upstream

--- components/utils/SearchBox.vue
+++ components/utils/SearchBox.vue
@@ -7,7 +7,7 @@
         class="bg-gray-200 text-3xl appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-200"
         type="text"
         placeholder="Search"
-        @input="queryUpdated"
+        @keyup="queryUpdated"
       />
     </form>
   </div>
